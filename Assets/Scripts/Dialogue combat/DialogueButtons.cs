7020016d00a57d75c30c8f718e1a75207536cb49
Jonassssssssssssss <113176035+Jonassssssssssssss@users.noreply.github.com>
2025-10-06 14:53:58 +0200

    buttons do something

--- Assets/Scripts/Dialogue combat/DialogueButtons.cs
+++ Assets/Scripts/Dialogue combat/DialogueButtons.cs
@@ -2,7 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.UI;
+using TMPro;
+
 public class DialogueButtons : MonoBehaviour
 {
     [SerializeField] string _message = "New message";
+    [SerializeField] TextMeshProUGUI _dialogueBubbleText;
+
+    public void ShowText()
+    {
+        _dialogueBubbleText.text = _message;
+    }
 }
