9d8f0c611a20a1bf342ca4ec0c7d9660ad6f023b
Jonassssssssssssss <113176035+Jonassssssssssssss@users.noreply.github.com>
2025-10-06 14:48:30 +0200

    added a dialogue bubble

---
+++ Assets/Scripts/Dialogue combat/DialogueButtons.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueButtons : MonoBehaviour
+{
+    [SerializeField] string _message = "New message";
+}
