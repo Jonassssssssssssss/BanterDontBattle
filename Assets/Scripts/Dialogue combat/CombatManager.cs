dfb7813eef1e4c9493fa1303081a19b8beb5c5cb
Jonassssssssssssss <113176035+Jonassssssssssssss@users.noreply.github.com>
2025-10-07 13:24:18 +0200

    push

---
+++ Assets/Scripts/Dialogue combat/CombatManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatManager : MonoBehaviour
+{
+    public int ProgressionPoints;
+
+    [SerializeField] GameObject[] ProgressionPointVisuals;
+
+    void Update()
+    {
+        if (ProgressionPoints == 5) Victory();
+
+        if (Input.GetKeyDown("x")) ProgressionPoints++;
+        if (Input.GetKeyDown("z")) ProgressionPoints--;
+
+        UpdateProgressionPointVisuals();
+    }
+
+    void Victory()
+    {
+        Debug.LogError("You Win");
+    }
+
+    void UpdateProgressionPointVisuals()
+    {
+        foreach (GameObject O in ProgressionPointVisuals)
+        {
+            O.SetActive(false);
+        }
+
+        if (ProgressionPoints >= 1) ProgressionPointVisuals[0].SetActive(true);
+
+        if (ProgressionPoints >= 2) ProgressionPointVisuals[1].SetActive(true);
+
+        if (ProgressionPoints >= 3) ProgressionPointVisuals[2].SetActive(true);
+
+        if (ProgressionPoints >= 4) ProgressionPointVisuals[3].SetActive(true);
+
+        if (ProgressionPoints >= 5) ProgressionPointVisuals[4].SetActive(true);
+    }
+}
