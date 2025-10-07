ded35db10cc3280993d6c53eaf22df7d95415ba8
NolleZero <104145010+NolleZero@users.noreply.github.com>
2025-10-07 11:38:47 +0200

    Movement scene progress 1
    made player and enemy who will fallow you

---
+++ Assets/Scripts/World movement/Enemy.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform playerTransform; // Assign the player's transform in the Inspector
+        public float moveSpeed = 3f;
+        private bool playerDetected = false;
+
+        public GameObject alert;
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                
+                playerDetected = true;
+                alert.SetActive(true);
+                
+                
+            }
+        }
+
+        
+
+        void Update()
+        {
+            if (playerDetected && playerTransform != null)
+            {
+                StartCoroutine(ExecuteAfterSeconds(3f)); // 3f for 3 seconds delay
+                //transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, moveSpeed * Time.deltaTime);
+            }
+        }
+
+        IEnumerator ExecuteAfterSeconds(float delayTime)
+        {
+            // Wait for the specified amount of seconds
+            yield return new WaitForSeconds(delayTime);
+            alert.SetActive(false);
+            transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, moveSpeed * Time.deltaTime);
+
+            
+        }
+}
